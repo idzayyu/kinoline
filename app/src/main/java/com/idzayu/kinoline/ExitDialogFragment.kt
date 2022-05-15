79a6ff2ab72c30d55c75cb75af7aa10464c494b1
idzayyu <idzayu@mail.ru>
2022-05-15 22:00:24 +0300

    Correction the dialogFragmentExit

--- app/src/main/java/com/idzayu/kinoline/ExitDialogFragment.kt
+++ app/src/main/java/com/idzayu/kinoline/ExitDialogFragment.kt
@@ -1,11 +1,15 @@
 package com.idzayu.kinoline
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import com.idzayu.kinoline.databinding.FragmentExitDialogBinding
+import com.idzayu.kinoline.databinding.FragmentMovieFavoriteBinding
 
 
 class ExitDialogFragment : DialogFragment() {
@@ -14,9 +18,20 @@ class ExitDialogFragment : DialogFragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_exit_dialog, container, false)
+    ): View {
+        val binding = FragmentExitDialogBinding.inflate(inflater)
+        init(binding)
+        dialog?.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return binding.root
     }
 
+    private fun init(binding: FragmentExitDialogBinding){
+        binding.buttonExit.setOnClickListener{
+            activity?.finish()
+        }
+        binding.buttonNoExit.setOnClickListener {
+            onStop()
+        }
+    }
 
 }
